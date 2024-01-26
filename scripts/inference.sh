66b91414e5d6c2ca4fad40f8f79491274a4cb783
piddnad <piddnad@gmail.com>
2024-01-27 00:52:27 +0800

    fix file name typo

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=0 \
-python3 inference/colorization_pipline.py \
+python3 inference/colorization_pipeline.py \
 	--input ./assets/test_images --output ./colorize_output \
 	--model_path modelscope/damo/cv_ddcolor_image-colorization/pytorch_model.pt
